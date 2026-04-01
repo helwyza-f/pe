252f85d42c8d1b466a3dc217a6e4c485f3eb28a8
helwyza <helwyza@gmail.com>
2026-04-01 21:31:56 +0700

    update api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { getCookie } from "cookies-next"; // Install: npm install cookies-next
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api.bookinaja.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
